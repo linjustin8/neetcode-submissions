b467ae0c3af40b1d293a30e78661202ba8aa24e7
Justin Lin <127445989+linjustin8@users.noreply.github.com>
2026-09-13 18:40:11 -0400

    Add: count-connected-components - submission-18

---
+++ Data Structures & Algorithms/count-connected-components/submission-18.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        s = set(range(n))
+        q = deque()
+        res = 0
+        
+        for i in range(n):
+            if i not in s:
+                continue
+            
+            res += 1
+            q.append(i)
+            while q:
+                curr = q.popleft()
+                if curr in s:
+                    s.remove(curr)
+                for neigh in adj[curr]:
+                    if neigh not in s:
+                        continue
+                    q.append(neigh)
+        
+        return res
