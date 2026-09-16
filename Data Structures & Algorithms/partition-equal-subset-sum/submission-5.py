12738739cec2bfb576aeb9d57d1e6c7697a28921
Justin Lin <127445989+linjustin8@users.noreply.github.com>
2026-09-15 20:25:35 -0400

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sm = sum(nums)
+        if sm % 2:
+            return False
+
+        target = sm // 2
+        dp = set()
+
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == target:
+                return True
+            
+            for n in list(dp):
+                curr = nums[i] + n
+                if curr == target:
+                    return True
+                dp.add(curr)
+            dp.add(nums[i])
+        
+        return False
